c350c4923d32afd76bac244e66c4816439782164
csuarez <claudia.suarez@mercadolibre.com>
2014-12-09 11:35:00 -0200

    Second commit

--- webserver/grails-app/controllers/VersionarController.groovy
+++ webserver/grails-app/controllers/VersionarController.groovy
@@ -1,20 +1,26 @@
+import groovy.json.*
 
 class VersionarController {
 
     def test() { 
 
+    	def json = request.JSON;
+
     	withJGit() { rf ->
 		    pull().call()
-		    def f = new File(rf,"test.txt")
-		    f.createNewFile()
+		    def f = new File(rf,"/configurations")
+		    f.createNewFile();
+		    f.write(new JsonBuilder(json).toPrettyString())
+		    f.renameTo("seo_configuration_MCO.json")
+		    
 		    // Relative path
 		    add().addFilepattern(f.name).call()
 		    if(!status().call().isClean()) {
-		        commit().setMessage("some comment").call()
+		        commit().setMessage("Versionando MCO").call()
 		    }
 		    push().call()
 		    pull().call()
 		}
-		render "hola"
+		render "listo"
     }
 }
